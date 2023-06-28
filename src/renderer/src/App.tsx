a68b06c0acc079702438673575436ab5673898c0
Misael Lopes <mecl.ely@gmail.com>
2023-06-28 03:19:49 +0100

    tailwind and postcss config

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,3 +1,5 @@
+import './styles/global.css'
+
 export function App(): JSX.Element {
-  return <h1>Hello World with Electron</h1>
+  return <h1 className='text-violet-400'>Hello World with Electron</h1>
 }

9fe8027d9caea4c6784c7ddd51be835ff6f5914e
Misael Lopes <mecl.ely@gmail.com>
2023-07-14 13:53:58 +0100

    setting up react-query

--- src/main/ipc.ts
+++ src/main/ipc.ts
@@ -1,7 +1,10 @@
 import { ipcMain } from 'electron'
 
-ipcMain.handle('fetch-documents', async (_, params) => {
-  console.log(params)
-
-  return 'Misael Lopes World'
+ipcMain.handle('fetch-documents', async () => {
+  return [
+    { id: '1', title: 'Ignite' },
+    { id: '2', title: 'Explorer' },
+    { id: '3', title: 'Discover' },
+    { id: '4', title: 'Rocketseat Plus' },
+  ]
 })
--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,13 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Routes } from './Routes'
 import './styles/global.css'
+import { queryClient } from './lib/react-query'
 
 // eslint-disable-next-line no-undef
 export function App() {
-  return <Routes />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Routes />
+    </QueryClientProvider>
+  )
 }

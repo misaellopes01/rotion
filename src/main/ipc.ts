197d2c5ca5e7161fca04450cb457bf82f8293dbd
Misael Lopes <mecl.ely@gmail.com>
2023-08-02 19:27:18 +0100

    crud in progress

--- src/main/ipc.ts
+++ src/main/ipc.ts
@@ -1,10 +1,45 @@
 import { ipcMain } from 'electron'
+import { IPC } from '@shared/constants/ipc'
+import {
+  CreateDocumentResponse,
+  DeleteDocumentRequest,
+  FetchAllDocumentsResponse,
+  FetchDocumentRequest,
+  FetchDocumentResponse,
+  SaveDocumentRequest,
+} from '@shared/types/ipc'
+import { store } from './store'
 
-ipcMain.handle('fetch-documents', async () => {
-  return [
-    { id: '1', title: 'Ignite' },
-    { id: '2', title: 'Explorer' },
-    { id: '3', title: 'Discover' },
-    { id: '4', title: 'Rocketseat Plus' },
-  ]
-})
+ipcMain.handle(
+  IPC.DOCUMENTS.FETCH_ALL,
+  async (): Promise<FetchAllDocumentsResponse> => {
+    return {
+      data: Object.values(store.get('documents')),
+    }
+  },
+)
+ipcMain.handle(
+  IPC.DOCUMENTS.FETCH,
+  async (_, { id }: FetchDocumentRequest): Promise<FetchDocumentResponse> => {
+    const document = store.get<string, Document>(`documents.${id}`)
+    return {
+      data: document,
+    }
+  },
+)
+ipcMain.handle(
+  IPC.DOCUMENTS.CREATE,
+  async (): Promise<CreateDocumentResponse> => {
+    return {
+      document: {},
+    }
+  },
+)
+ipcMain.handle(
+  IPC.DOCUMENTS.SAVE,
+  async ({ id }: SaveDocumentRequest): Promise<void> => {},
+)
+ipcMain.handle(
+  IPC.DOCUMENTS.DELETE,
+  async ({ id }: DeleteDocumentRequest): Promise<void> => {},
+)
--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,5 +1,14 @@
 import { contextBridge, ipcRenderer } from 'electron'
 import { ElectronAPI, electronAPI } from '@electron-toolkit/preload'
+import { IPC } from '@shared/constants/ipc'
+import {
+  CreateDocumentResponse,
+  DeleteDocumentRequest,
+  FetchAllDocumentsResponse,
+  FetchDocumentRequest,
+  FetchDocumentResponse,
+  SaveDocumentRequest,
+} from '@shared/types/ipc'
 
 declare global {
   export interface Window {
@@ -10,8 +19,26 @@ declare global {
 
 // Custom APIs for renderer
 const api = {
-  async fetchDocuments(): Promise<Array<{ id: string; title: string }>> {
-    return ipcRenderer.invoke('fetch-documents')
+  async fetchDocuments(): Promise<FetchAllDocumentsResponse> {
+    return ipcRenderer.invoke(IPC.DOCUMENTS.FETCH_ALL)
+  },
+
+  async fetchDocument(
+    request: FetchDocumentRequest,
+  ): Promise<FetchDocumentResponse> {
+    return ipcRenderer.invoke(IPC.DOCUMENTS.FETCH, request)
+  },
+
+  async createDocument(): Promise<CreateDocumentResponse> {
+    return ipcRenderer.invoke(IPC.DOCUMENTS.CREATE)
+  },
+
+  async saveDocument(request: SaveDocumentRequest): Promise<void> {
+    return ipcRenderer.invoke(IPC.DOCUMENTS.SAVE, request)
+  },
+
+  async deleteDocument(request: DeleteDocumentRequest): Promise<void> {
+    return ipcRenderer.invoke(IPC.DOCUMENTS.SAVE, request)
   },
 }
 
